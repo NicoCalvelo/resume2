3be71af19d596df41a9dac5e2aa3c29c38ae5208
Nico Calvelo <dia.calvelo.nicolas@gmail.com>
2024-05-17 14:37:36 +0200

    modifs

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
+import React from "react";
 import Home from "./pages/home/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HistoryProvider from "./providers/HistoryProvider";
-import Dashboard from "./pages/recipes/Dashboard";
-
-import "./base/Providers/ConfirmationDialogsProvider";
+import ReactDOM from "react-dom/client";
 import "./base/Providers/ToastsProvider";
+import Dashboard from "./pages/recipes/Dashboard";
 import RecipesList from "./pages/recipes/RecipesList";
+import "./base/Providers/ConfirmationDialogsProvider";
+import HistoryProvider from "./providers/HistoryProvider";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
